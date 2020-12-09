68a834e7e4168f92a5b5d8161f2a7b04aae4b9a4
OliverRai <raiane.mateus.oliveira@gmail.com>
2020-12-09 16:11:53 -0300

    melhorando quicksort

--- Ordenação - Quicksort - MergeSort/quickSort.c
+++ Ordenação - Quicksort - MergeSort/quickSort.c
@@ -1,71 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int particiona(int *vetor, int inicio, int final){
-    int esquerda, direita, pivo, aux;
-    esquerda = inicio;
-    direita = final;
-
-    pivo = vetor[inicio];
-
-    while(esquerda < direita){
-        while (vetor[esquerda] <= pivo){ //avança a posição da esquerda
-            esquerda++;
-        }
-        while(vetor[direita] > pivo){//recua a posição da direita
-            direita--;
-        }
-        if(esquerda < direita){ // troca esquerda e direita
-            aux = vetor[esquerda];
-            vetor[esquerda] = vetor[direita];
-            vetor[direita] = aux;
-        }
-    }
-    vetor[inicio] = vetor[direita];
-    vetor[direita] = pivo;
-    return direita;
-}
-
-void ordenarQuicksort(int vetor[], int inicio, int fim)
+void troca(int *vetor, int esquerda, int direita)
 {
-   int pivo;
-   if(fim > inicio){
-       pivo = particiona(vetor, inicio, fim); // pra pegar a posição do pivo
-       ordenarQuicksort(vetor, inicio, pivo - 1); 
-       ordenarQuicksort(vetor, pivo + 1, fim);
-   }
+  int aux;
+
+  aux = vetor[esquerda];
+  vetor[esquerda] = vetor[direita];
+  vetor[direita] = aux;
 }
 
 void show(int *vet, int size)
-{   
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        printf("Numero --->  %d \n",  vet[i]);
+{
+  int i;
+  for (i = 0; i < size; i++)
+  {
+    printf("Numero --->  %d \n", vet[i]);
+  }
+}
+
+int particionar(int *vetor, int inicio, int fim){
+  int pivo = vetor[inicio]; //primeiro elemento é o pivo
+  int i = inicio + 1; //índice i faz a varredura da esquerda para direita
+  int j = fim; //índice j faz a varredura da direita para esquerda
+  while(i <= j){////enquanto os índice não se ultrapassarem
+    while(i <= j && vetor[i] <= pivo){  //enquanto vetor[i] não for maior que o pivô
+      i++;
     }
+    while(i <= j && vetor[j] > pivo){//enquanto vetor[j] não for menor ou igual ao pivô
+      j--;
+    }
+    if(i < j) troca(vetor, i, j); //se os índices não se ultrapassarem, troque os elementos
+  }
+  troca(vetor, inicio, j);//coloca o pivô na posição de ordenação
+  return j;
+}
+
+void ordenarQuicksort(int *v, int esquerda, int direita)
+{
+  int pivo;
+
+  if (direita <= esquerda) return; //verifica se o vetor tem um ou zero elementos é pq já ta ordenado
+
+  pivo = particionar(v, esquerda, direita); //para dividir o vetor
+  ordenarQuicksort(v, esquerda, pivo - 1);  //esquerda do pivo, do começo até pivo - 1
+  ordenarQuicksort(v, pivo + 1, direita);   //direita do pivo
 }
 
 int main()
 {
-    int size, i, *numbersVetor;
+  int size, i, *numbersVetor;
 
-    printf("Informe o tamanho do vetor: ");
-    scanf("%d", &size);
+  printf("Informe o tamanho do vetor: ");
+  scanf("%d", &size);
 
-    numbersVetor = (int *) malloc(size * sizeof(int));
+  numbersVetor = (int *)malloc(size * sizeof(int));
 
-    for (i = 0; i < size; i++)
-    {
-        numbersVetor[i] = 1 + rand() % 100;
-    }
-    printf("** Numero desordenados ** \n");
+  for (i = 0; i < size; i++)
+  {
+    numbersVetor[i] = 1 + rand() % 100;
+  }
+  printf("** Numero desordenados ** \n");
 
-    show(numbersVetor, size);
+  show(numbersVetor, size);
 
-    ordenarQuicksort(numbersVetor, 0, size);
+  ordenarQuicksort(numbersVetor, 0, size);
 
-    printf("** Numero ordenados ** \n");
-    show(numbersVetor, size);
+  printf("** Numero ordenados ** \n");
+  show(numbersVetor, size);
 
-    system("pause");
-    return 0;
+  free(numbersVetor);
+  system("pause");
+  return 0;
 }
