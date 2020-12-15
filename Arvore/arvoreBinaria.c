4a67be4e597e065d35548d0400e9ea3c0d97a719
OliverRai <raiane.mateus.oliveira@gmail.com>
2020-12-15 18:57:35 -0300

    arvore binaria finalizada

--- Arvore/arvoreBinaria.c
+++ Arvore/arvoreBinaria.c
@@ -4,12 +4,13 @@
 
 typedef struct node
 {
-  int info; //valor há ser inserido
+  int info;       //valor há ser inserido
   struct NO *esq; // crio esquerda e direita na mesma estrutura
   struct NO *dir;
-}NO;
+} NO;
 
-typedef struct tree{
+typedef struct tree
+{
   NO *raiz;
 } Arvore;
 
@@ -25,23 +26,23 @@ Arvore *criararvore()
   return arv;
 }
 
-void destroy(NO *raiz){
-  if(raiz != NULL){
-    destroy(raiz->esq);
-    destroy(raiz->dir);
-    free(raiz);
-  }
-}
+// void destroy(NO *raiz){
+//   if(raiz != NULL){
+//     destroy(raiz->esq);
+//     destroy(raiz->dir);
+//     free(raiz);
+//   }
+// }
 
-void destryTree(Arvore *arv){
-  if(arv == NULL){
-    return;
-  }
-  destroy(arv->raiz);
-  free(arv);
-}
+// void destroyTree(Arvore *arv){
+//   if(arv == NULL){
+//     return;
+//   }
+//   destroy(arv->raiz);
+//   free(arv);
+// }
 
- //Funcao que verifica se arvore esta vazia
+//Funcao que verifica se arvore esta vazia
 int vazia(Arvore *arv)
 {
   if (arv->raiz == NULL)
@@ -49,11 +50,11 @@ int vazia(Arvore *arv)
     printf("Arvore vazia!\n");
     return 1;
   }
- // if (*raiz == NULL)
- // {
- //   printf("Arvore vazia!\n");
- //   return 1;
- // }
+  // if (*raiz == NULL)
+  // {
+  //   printf("Arvore vazia!\n");
+  //   return 1;
+  // }
   printf("Arvore não vazia!\n");
   return 0;
 }
@@ -68,7 +69,7 @@ void preOrdem(Arvore *arv)
   if (arv->raiz != NULL)
   {
     printf("%d\n", arv->raiz->info); //printa o que tem dentro do
-    preOrdem(&((arv->raiz->esq)));     // vai percorrendo pela esquerda até que toda a arvore seja percorrida
+    preOrdem(&((arv->raiz->esq)));   // vai percorrendo pela esquerda até que toda a arvore seja percorrida
     preOrdem(&((arv->raiz->dir)));
   }
 }
@@ -103,19 +104,27 @@ void posOrdem(Arvore *arv)
   }
 }
 
-void add(NO *raiz, NO *novo){
-  if(raiz->info > novo->info){
-    if(raiz->esq == NULL){
+void add(NO *raiz, NO *novo)
+{
+  if (raiz->info > novo->info)
+  {
+    if (raiz->esq == NULL)
+    {
       raiz->esq = novo;
-    } else {
+    }
+    else
+    {
       add(raiz->esq, novo);
     }
   }
-  else if(raiz->info < novo->info){
-    if(raiz->dir == NULL){
+  else if (raiz->info < novo->info)
+  {
+    if (raiz->dir == NULL)
+    {
       raiz->dir = novo;
     }
-    else {
+    else
+    {
       add(raiz->dir, novo);
     }
   }
@@ -126,111 +135,116 @@ int insere(Arvore *arv, int valor)
   if (arv == NULL)
   {
     return 0;
-  }                //verifica se esta fazia
+  }         //verifica se esta fazia
   NO *novo; //cria um no auxiliar
-  novo = (NO*)malloc(sizeof(NO));
-  if(novo == NULL){
+  novo = (NO *)malloc(sizeof(NO));
+  if (novo == NULL)
+  {
     return;
   }
   novo->esq = novo->dir = NULL;
   novo->info = valor;
 
-  if(arv->raiz == NULL){
+  if (arv->raiz == NULL)
+  {
     arv->raiz = novo;
   }
-  else {
+  else
+  {
     add(arv->raiz, novo);
   }
 }
 
-NO *removendo_atual(NO *atual)
-{
-  NO *no1, *no2;
-  if (atual->esq == NULL)
-  {
-    no2 = atual->dir;
-    free(atual);
-    return no2;
-  }
-  no1 = atual;
-  no2 = atual->esq;
-  while (no2->dir != NULL)
-  {
-    no1 = no2;
-    no2 = no2->dir;
-  }
-
-  if (no1 != atual)
-  {
-    no1->dir = no2->esq;
-    no2->esq = atual->esq;
-  }
-  no2->dir = atual->dir;
-  free(atual);
-  return no2;
+NO *MaiorDireita(NO **no){
+   if((*no)->dir != NULL) 
+       return MaiorDireita(&(*no)->dir);
+    else{
+      NO *aux = *no;
+      if((*no)->esq != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esquerda!
+           *no = (*no)->esq;
+        else
+           *no = NULL;
+        return aux;
+    }
 }
 
-//Função que remove um elemento da árvore binária
-int remover(Arvore *arv, int valor)
+void remover(NO **pRaiz, int numero)
 {
-  if (arv->raiz == NULL)
-  {
-    return 0;
+  if (*pRaiz == NULL)
+  { // esta verificacao serve para caso o numero nao exista na arvore.
+    printf("Numero nao existe na arvore!");
+    getch(); // getch() é fogo, mas tudo bem
+    return;
   }
-  NO *ant = NULL;    //variavel auxiliar
-  NO *atual = arv->raiz; //o atual recebe a raiz para poder percorrer a arvore toda
-  while (atual != NULL)
-  { //enquanto ele for nu, ele continua no laço
-    if (valor == atual->info)
-    {
-      if (atual == arv->raiz)
-      {
-        arv->raiz = removendo_atual(atual);
-        //faz uma chamada recursiva para remover o no ja achado
-      }
-      else
-      {
-        if (ant->dir == atual)
-        { //faz a comparação se for maior remove pela a direita usando recursividade, mesmo pensamento do inserir
-          ant->dir = removendo_atual(atual);
-        }
-        else
-        { //mesmo procedimento
-          ant->esq = removendo_atual(atual);
-        }
-      }
-      return 1;
-    }
-    ant = atual;
-    if (valor > atual->info)
-    {
-      atual = atual->dir; //realocando os nos depois de excluir
+
+  if (numero < (*pRaiz)->info)
+    remover(&(*pRaiz)->esq, numero);
+  else if (numero > (*pRaiz)->info)
+    remover(&(*pRaiz)->dir, numero);
+  else
+  {                    // se nao eh menor nem maior, logo, eh o numero que estou procurando! :)
+    NO *pAux = *pRaiz; // quem programar no Embarcadero vai ter que declarar o pAux no inicio do void! :[
+    if (((*pRaiz)->esq == NULL) && ((*pRaiz)->dir == NULL))
+    { // se nao houver filhos...
+      free(pAux);
+      (*pRaiz) = NULL;
     }
     else
-    { //realocando os nos depois de excluir
-      atual = atual->esq;
+    { // so tem o filho da direita
+      if ((*pRaiz)->esq == NULL)
+      {
+        (*pRaiz) = (*pRaiz)->dir;
+        pAux->dir = NULL;
+        free(pAux);
+        pAux = NULL;
+      }
+      else
+      { //so tem filho da esquerda
+        if ((*pRaiz)->dir == NULL)
+        {
+          (*pRaiz) = (*pRaiz)->esq;
+          pAux->esq = NULL;
+          free(pAux);
+          pAux = NULL;
+        }
+        else
+        {                                           //Escolhi fazer o maior filho direito da subarvore esquerda.
+          pAux = MaiorDireita(&(*pRaiz)->esq); //se vc quiser usar o Menor da esquerda, so o que mudaria seria isso:
+          pAux->esq = (*pRaiz)->esq;      //        pAux = MenorEsquerda(&(*pRaiz)->direita);
+          pAux->dir = (*pRaiz)->dir;
+          (*pRaiz)->esq = (*pRaiz)->esq = NULL;
+          free((*pRaiz));
+          *pRaiz = pAux;
+          pAux = NULL;
+        }
+      }
     }
   }
 }
 
 int main()
 {
-    Arvore *arv = criararvore();
-    insere(arv, 5);
-    insere(arv, 2);
-    insere(arv, 6);
-    insere(arv, 8);
-    insere(arv, 3);
-    insere(arv, 10);
-    preOrdem(arv);
-    printf("\n");
-    emOrdem(arv);
-    printf("\n");
-    posOrdem(arv);
-    printf("\n");
-    remover(arv, 10);
-    emOrdem(arv);
-  
+  Arvore *arv = criararvore();
+  insere(arv, 8);
+  insere(arv, 4);
+  insere(arv, 12);
+  insere(arv, 2);
+  insere(arv, 6);
+  insere(arv, 10);
+  insere(arv, 14);
+  insere(arv, 1);
+  insere(arv, 3);
+  insere(arv, 5);
+  insere(arv, 11);
+  insere(arv, 13);
+  insere(arv, 15);
+
+  preOrdem(arv);
+  printf("\n");
+  printf("\n");
+  remover(arv, 4);
+  preOrdem(arv);
+
   system("pause");
   return 0;
 }
